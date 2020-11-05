76106b2cd98255ae2594c946fa25705331899995
Malte <malte.hain@t-onlien.de>
2020-11-05 09:42:50 +0100

    change by malte

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,6 +3,7 @@ public class HelloWorld {
 	public static void main(String [] args) {
 		System.out.println("Hallo Welt! Mein Name ist Anton!");
 		System.out.println("Mein Geburtstag ist de 07.02.");
+		System.out.println("Hello from Malte");
 	}
 
 }
